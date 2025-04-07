c723c2797e9ffc8f69fdd986b025db7b9382fbef
Tong Shen <tongs@amazon.com>
2025-04-07 22:06:03 +0800

    Update sample node app import path

--- javascript/src/sample-node-app/index.js
+++ javascript/src/sample-node-app/index.js
@@ -1,10 +1,10 @@
 import { AppConfig } from './app.config.mjs';
-import { LwaAuthClient } from 'amazon-sp-api-sdk-js/src/helper/LwaAuthClient.mjs';
+import { LwaAuthClient } from '@amazon-sp-api-release/amazon-sp-api-sdk-js/src/helper/LwaAuthClient.mjs';
 
 import {
   SellersApi,
   ApiClient as SellersApiClient,
-} from 'amazon-sp-api-sdk-js/sdk/sellers_v1/src/index.js';
+} from '@amazon-sp-api-release/amazon-sp-api-sdk-js/sdk/sellers_v1/src/index.js';
 
 (async () => {
   const lwaClient = new LwaAuthClient(
