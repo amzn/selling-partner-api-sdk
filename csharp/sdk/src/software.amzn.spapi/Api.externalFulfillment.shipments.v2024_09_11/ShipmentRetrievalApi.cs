9a89e83cc80f6f2a9ccf2271517949d8f918f637
mafge <175320164+mafge@users.noreply.github.com>
2026-04-10 22:29:14 +0000

    Update Csharp SDK

--- csharp/sdk/src/software.amzn.spapi/Api.externalFulfillment.shipments.v2024_09_11/ShipmentRetrievalApi.cs
+++ csharp/sdk/src/software.amzn.spapi/Api.externalFulfillment.shipments.v2024_09_11/ShipmentRetrievalApi.cs
@@ -54,7 +54,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         /// Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </remarks>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -72,7 +72,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         /// Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </remarks>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -112,7 +112,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         /// Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </remarks>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -130,7 +130,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         /// Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </remarks>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -363,7 +363,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         ///  Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </summary>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -382,7 +382,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         ///  Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </summary>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -448,7 +448,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         ///  Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </summary>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
@@ -468,7 +468,7 @@ namespace software.amzn.spapi.Api.externalFulfillment.shipments.v2024_09_11
         ///  Get a list of shipments created for the seller in the status you specify. Shipments can be further filtered based on the fulfillment node or the time of the shipments&#39; last update.
         /// </summary>
         /// <exception cref="software.amzn.spapi.Client.ApiException">Thrown when fails to make API call</exception>
-        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;ACCEPTED&#x60;.</param>
+        /// <param name="status">The status of shipment you want to include in the response. To retrieve all new shipments, set this value to &#x60;CREATED&#x60; or &#x60;ACCEPTED&#x60;.</param>
         /// <param name="locationId">The Amazon channel location identifier for the shipments you want to retrieve. (optional)</param>
         /// <param name="marketplaceId">The marketplace ID associated with the location. To find the ID for your marketplace, refer to [Marketplace IDs](https://developer-docs.amazon.com/sp-api/docs/marketplace-ids). (optional)</param>
         /// <param name="channelName">The channel name associated with the location. (optional)</param>
