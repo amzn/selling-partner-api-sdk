6d7522fad85d214d6158d4ca7641b9770c9dc298
mafge <mfge@amazon.de>
2026-08-11 13:40:53 +0200

    Remove lombok

--- java/sdk/src/main/java/com/amazon/SellingPartnerAPIAA/LWAClientScopes.java
+++ java/sdk/src/main/java/com/amazon/SellingPartnerAPIAA/LWAClientScopes.java
@@ -2,15 +2,19 @@ package com.amazon.SellingPartnerAPIAA;
 
 import com.google.gson.annotations.JsonAdapter;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
 @JsonAdapter(LWAClientScopesSerializerDeserializer.class)
 public class LWAClientScopes {
 
     private final Set<String> scopes;
+
+    public LWAClientScopes(Set<String> scopes) {
+        this.scopes = scopes;
+    }
+
+    public Set<String> getScopes() {
+        return scopes;
+    }
 
     protected void addScope(String scope) {
         scopes.add(scope);
